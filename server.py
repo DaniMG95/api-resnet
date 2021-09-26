a3a083b3891bf7c4e7cb67ff81462d28d57476a7
DaniMG95 <dani_16_5_95@hotmail.com>
2021-09-26 11:49:12 +0200

    Create server with endpoints

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
-from resources.predict import Predict
+from resources.predict import PredictImage, PredictURL
 
-app = Flask(__name__)
-api = Api(app)
-
-api.add_resource(Predict, '/')
+def create_api():
+    _app = Flask(__name__)
+    api = Api(_app, prefix='/api/')
+    CORS(_app)
+    api.add_resource(PredictImage, '/image/')
+    api.add_resource(PredictURL, '/url/')
+    return _app
 
 if __name__ == '__main__':
+
+    app = create_api()
     app.run(debug=True)
