639792047bdaa8a2addb9356d63ce5555dbc1031
DaniMG95 <dani_16_5_95@hotmail.com>
2021-09-26 12:20:30 +0200

    output correct and required data body

--- resources/predict.py
+++ resources/predict.py
@@ -1,27 +1,24 @@
 from flask_restful import Resource, reqparse
 from models.predictUrl import PredictUrlModel
 
+
 class PredictImage(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('img', type=str, help='img to charge for this resource')
+    parser.add_argument('img', type=str, help='img to charge for this resource', required=True)
 
     @classmethod
     def get(cls):
         image_base64 = cls.parser.parse_args()['img']
         predictor = PredictUrlModel(image_base64)
-        return predictor.predict_image()
-
+        return {"class": predictor.predict_image()}
 
 
 class PredictURL(Resource):
-
     parser = reqparse.RequestParser()
-    parser.add_argument('url', type=str, help='URl image to charge for this resource')
+    parser.add_argument('url', type=str, help='URl image to charge for this resource', required=True)
 
     @classmethod
     def get(cls):
         url_image = cls.parser.parse_args()['url']
         predictor = PredictUrlModel(url_image)
-        return predictor.predict_image()
-
-
+        return {"class": predictor.predict_image()}
