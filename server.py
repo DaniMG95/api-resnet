b779dfa812318a728f88f5429f34e3c1424d002c
DaniMG95 <dani_16_5_95@hotmail.com>
2021-09-26 11:18:44 +0200

    Api RestFull init

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_restful import Api
+from resources.predict import Predict
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Predict, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
